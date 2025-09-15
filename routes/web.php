43f8aa78727d85bdbe693da8cf6fd3c703825b90
MKashanButt <kashanbutt566@gmail.com>
2025-09-15 23:04:12 +0500

    add group controller on dme-equipment page and add search route

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'app.index')->name('app.home');
 
-Route::get('/dme-equipments', [DMEEquipmentController::class, 'index'])->name('app.products');
+Route::controller(DMEEquipmentController::class)->group(function () {
+    Route::get('/dme-equipments', 'index')->name('app.products');
+    Route::get('/dme-equipments/search', 'search')->name('app.products-search');
+});
 
 Route::view('/caregivers', 'app.caregivers')->name('app.caregivers');
 
@@ -24,41 +27,41 @@ Route::view('/login-register', 'app.login-register')
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::controller(DashboardRouteRenderController::class)
-    ->prefix('/user')
-    ->group(function () {
-        Route::get('/dashboard', 'index')->name('user.dashboard');
-        Route::get('/cart', 'cart')->name('user.cart');
-        Route::get('/orders', 'orders')->name('user.orders');
-        Route::get('/order-history', 'orderHistory')->name('user.order-history');
-    });
-
-    Route::controller(CartController::class)
-    ->prefix('/user/cart')
-    ->group(function () {
-        Route::get('/store/{id}', 'store')->name('user.cart.store');
-        Route::get('/destroy/{id}', 'destroy')->name('user.cart.destroy');
-        Route::get('/clear', 'clear')->name('user.cart.clear');
-    });
-
-    Route::controller(OrderController::class)
-    ->prefix('/user')
-    ->group(function () {
-        Route::get('/order/create/{id}', 'create')->name('order.create');
-        Route::get('/order/store/{id}', 'store')->name('order.store');
-    });
-
-    Route::prefix('/admin')->middleware('admin')->group(function() {
-        Route::controller(AdminController::class)
-        ->group(function() {
-            Route::get('/','index')->name('admin.dashboard');
+        ->prefix('/user')
+        ->group(function () {
+            Route::get('/dashboard', 'index')->name('user.dashboard');
+            Route::get('/cart', 'cart')->name('user.cart');
+            Route::get('/orders', 'orders')->name('user.orders');
+            Route::get('/order-history', 'orderHistory')->name('user.order-history');
         });
 
+    Route::controller(CartController::class)
+        ->prefix('/user/cart')
+        ->group(function () {
+            Route::get('/store/{id}', 'store')->name('user.cart.store');
+            Route::get('/destroy/{id}', 'destroy')->name('user.cart.destroy');
+            Route::get('/clear', 'clear')->name('user.cart.clear');
+        });
+
+    Route::controller(OrderController::class)
+        ->prefix('/user')
+        ->group(function () {
+            Route::get('/order/create/{id}', 'create')->name('order.create');
+            Route::get('/order/store/{id}', 'store')->name('order.store');
+        });
+
+    Route::prefix('/admin')->middleware('admin')->group(function () {
+        Route::controller(AdminController::class)
+            ->group(function () {
+                Route::get('/', 'index')->name('admin.dashboard');
+            });
+
         Route::controller(ProductController::class)->group(function () {
-            Route::get('/create','create')->name('admin.create');
+            Route::get('/create', 'create')->name('admin.create');
             Route::post('/create', 'store')->name('admin.store');
-            
-            Route::get('/edit/{id}','edit')->name('admin.edit');
-            Route::put('/update/{id}','update')->name('admin.update');
+
+            Route::get('/edit/{id}', 'edit')->name('admin.edit');
+            Route::put('/update/{id}', 'update')->name('admin.update');
 
             Route::get('/destroy/{id}', 'destroy')->name('admin.destroy');
             Route::get('/clear-list', 'clearList')->name('admin.clearList');
