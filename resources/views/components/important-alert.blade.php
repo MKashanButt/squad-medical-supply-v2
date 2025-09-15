eda3567a9f92e31576b3e0bdd0511df4a0e4a794
MKashanButt <kashanbutt566@gmail.com>
2025-09-15 23:03:39 +0500

    add toggle display on alert

--- resources/views/components/important-alert.blade.php
+++ resources/views/components/important-alert.blade.php
@@ -1,9 +1,9 @@
-<div class="importantInfo" x-data="{ toggleDisplay: true }" x-show="toggleDisplay">
+<div class="importantInfo" id="importantInfo">
     <p>Important Information in regard of COVID19</p>
     <div class="infoButtons">
-        <button @click="toggleDisplay = false">Close</button>
+        <button id="close" onclick="toggleDisplay('importantInfo', 'close')">Close</button>
         <x-a href='/blogs/covid-19'>
-            <button @click="toggleDisplay = false">Read</button>
+            <button>Read</button>
         </x-a>
     </div>
 </div>
