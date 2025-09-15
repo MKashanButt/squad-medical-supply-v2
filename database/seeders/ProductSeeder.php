8dcf47cceea267a1a85877f2d480d0538cd5cbea
MKashanButt <kashanbutt566@gmail.com>
2025-09-15 23:02:12 +0500

    add json encode and converted tag string to array

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,7 +20,7 @@ class ProductSeeder extends Seeder
                 "key_benefits" => "Enables mobility for individuals with limited movement,Manual or powered options available,Designed for comfort and ease of use",
                 "description" =>
                 "Discover our range of comfortable and durable wheelchairs, designed for ease of use and enhanced mobility. Choose the perfect model to fit your lifestyle and needs.",
-                'tags' => "mobility, support, wheelchair",
+                'tags' => json_encode(["mobility", "support", "wheelchair"]),
             ],
             [
                 "thumbnail" => "images/cgm-monitors.avif",
@@ -28,7 +28,7 @@ class ProductSeeder extends Seeder
                 'key_benefits' => "Continuous glucose monitoring,Tracks blood sugar levels in real-time,Sends alerts for high or low readings",
                 "description" =>
                 "Stay on top of your health with our continuous glucose monitors, providing real-time blood sugar data for optimal diabetes management. Easy to use and highly accurate.",
-                'tags' => "glucose, monitoring, health",
+                'tags' => json_encode(["glucose", "monitoring", "health"]),
             ],
             [
                 'thumbnail' => 'images/knee-braces.avif',
@@ -36,7 +36,7 @@ class ProductSeeder extends Seeder
                 'key_benefits' => "Provides support for injured or weak knees, Reduces knee strain during physical activities, Helps alleviate knee pain and inflammation",
                 'description' =>
                 'Support and stabilize your joints with our high-quality braces, designed for maximum comfort and effectiveness. Ideal for injury recovery and daily use.',
-                'tags' => "support, knee, brace",
+                'tags' => json_encode(["support", "knee", "brace"]),
             ],
             [
                 'thumbnail' => 'images/hip-braces.avif',
@@ -44,7 +44,7 @@ class ProductSeeder extends Seeder
                 'key_benefits' => "Supports the hip joint,Ideal for post-surgery recovery,Reduces hip strain during movement",
                 'description' =>
                 'Support and stabilize your joints with our high-quality braces, designed for maximum comfort and effectiveness. Ideal for injury recovery and daily use.',
-                'tags' => "support, hip, brace",
+                'tags' => json_encode(["support", "hip", "brace"]),
             ],
             [
                 'thumbnail' => 'images/back-braces.avif',
@@ -52,7 +52,7 @@ class ProductSeeder extends Seeder
                 'key_benefits' => "Elastic sleeve for joint support,Improves circulation,Ideal for compression therapy",
                 'description' =>
                 'Relieve back pain and improve posture with our supportive back braces. Designed to provide compression, stability, and comfort for daily wear or injury recovery.',
-                'tags' => "support, back, brace, posture",
+                'tags' => json_encode(["support", "back", "brace", "posture"]),
             ],
             [
                 "thumbnail" => 'images/uvb-light-wands.avif',
@@ -60,7 +60,7 @@ class ProductSeeder extends Seeder
                 "key_benefits" => "Portable UVB therapy for skin conditions,Effective for psoriasis treatment,Delivers controlled UVB exposure",
                 "description" =>
                 "Treat skin conditions effectively with portable UVB light wands. Perfect for psoriasis and other skin therapies, offering safe, targeted UVB exposure at home.",
-                "tags" => "therapy, UVB, psoriasis, skin treatment",
+                "tags" => json_encode(["therapy", "UVB", "psoriasis", "skin treatment"]),
             ],
             [
                 "thumbnail" => "images/tens-unit.avif",
@@ -68,7 +68,7 @@ class ProductSeeder extends Seeder
                 "key_benefits" => "Transcutaneous Electrical Nerve Stimulation device,Helps relieve pain through electric pulses,Portable and easy to use",
                 "description" =>
                 "Experience drug-free pain relief with our TENS units, delivering gentle electrical stimulation to soothe sore muscles and nerves. Compact, portable, and easy to use anytime.",
-                "tags" => "pain relief, TENS, therapy, muscle",
+                "tags" => json_encode(["pain relief", "TENS", "therapy", "muscle"]),
             ],
             [
                 "thumbnail" => "images/lymphedema-pump-massagers.avif",
@@ -76,7 +76,7 @@ class ProductSeeder extends Seeder
                 "key_benefits" => "Promotes lymphatic drainage,Reduces swelling and improves circulation,Adjustable pressure levels",
                 "description" =>
                 "Improve circulation and reduce swelling with lymphedema pump massagers. Designed to promote lymphatic drainage and provide adjustable comfort for effective therapy.",
-                "tags" => "therapy, lymphedema, circulation, massage",
+                "tags" => json_encode(["therapy", "lymphedema", "circulation", "massage"]),
             ],
 
         ]);
